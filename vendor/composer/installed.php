b2417a085269b32c8f74554259e674e41b5fb20e
Weverton-ghost <wevertonlrvinacio2@gmail.com>
2026-03-04 14:05:17 -0300

    Criação do arquivo composer e instalação dos pacotes, e configuração do autoload de classes.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdev/vagas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3adc9f7023e1473a1773c0ba884078b2d7aa456c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdev/vagas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3adc9f7023e1473a1773c0ba884078b2d7aa456c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
